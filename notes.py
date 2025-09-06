f362f5fa92b97cafac20cc237c5e939f0a50b0c1
hliu02 <haobo.liu@yahooinc.com>
2025-09-06 18:52:07 +0100

    add other endpoints

--- notes.py
+++ notes.py
@@ -25,6 +25,34 @@ def create_note(note: Note):
   return {"message": "Note created", "note": note}
 
 
+@notes_router.get("/notes")
+def get_all_notes():
+  return notes_dict.values()
+
+
+@notes_router.get("/notes/{note_id}")
+def get_note(note_id: int):
+  validate_note_id(note_id)
+
+  return notes_dict[note_id]
+
+
+@notes_router.put("/notes/{note_id}")
+def update_note(note_id: int, updated_note: Note):
+  validate_note_id(note_id)
+
+  notes_dict[note_id] = updated_note
+  return {"message": "Note updated", "note": updated_note}
+
+
+@notes_router.delete("/notes/{note_id}")
+def delete_note(note_id: int):
+  validate_note_id(note_id)
+
+  removed_note = notes_dict.pop(note_id)
+  return {"message": "Note deleted", "note": removed_note}
+
+
 def validate_note_id(note_id: int):
   if note_id not in notes_dict:
     raise HTTPException(status_code=404,
